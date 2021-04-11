c38dbef5f185b0fcc22f5c470cdf03ac02a01e8a
kmlr81 <kristina.mueller02@gmail.com>
2021-04-11 15:36:21 +0200

    quick fix for bash output in merge_reads.py

--- bin/scripts/merge_reads.py
+++ bin/scripts/merge_reads.py
@@ -62,144 +62,145 @@ def merge_all(linkage_table_path, chrom_sizes_paths, allowed_file_formats,
 
     pairs = find_pairs(linkage_frame)
     print("{} pairs were found".format(len(pairs)))
+    
+    if len(pairs) > 0:
+        pairs_merge = []
+        converted_idxs = []
+        merged_files = []
+        old_files = []
+        rows = []
 
-    pairs_merge = []
-    converted_idxs = []
-    merged_files = []
-    old_files = []
-    rows = []
+        # If file format of forward/reverse reads is bedGraph, then convert to
+        # bigWig for merging tool and make new pairs_merge List with only bigWig
+        # file paths for merging
+        print("------ Converting bedGraph files to bigWig for merging ------")
 
-    # If file format of forward/reverse reads is bedGraph, then convert to
-    # bigWig for merging tool and make new pairs_merge List with only bigWig
-    # file paths for merging
-    print("------ Converting bedGraph files to bigWig for merging ------")
-
-    for idx, pair in enumerate(pairs):
-        tmp_pair = []
-        for i in range(0, len(pair)):
-            if os.path.exists(pair[i]):
-                file_ext = os.path.splitext(pair[i])[1].lower()
-                if ".bedgraph" == file_ext:
-                    print("Converting file: " + pair[i])
-                    genome = linkage_frame.loc[linkage_frame["file_path"] ==
-                                               pair[i]]["genome"]
-                    # Find the right chrom.sizes file for genome
-                    chrom_sizes = [el for el in chrom_sizes_paths if
-                                   genome.iloc[0] in el]
-                    bw_file_path = convert_bedgraph_to_bigwig(pair[i],
-                                                              chrom_sizes[0],
-                                                              conversion_tool)
-                    if not os.path.exists(bw_file_path):
-                        sort_file(pair[i])
+        for idx, pair in enumerate(pairs):
+            tmp_pair = []
+            for i in range(0, len(pair)):
+                if os.path.exists(pair[i]):
+                    file_ext = os.path.splitext(pair[i])[1].lower()
+                    if ".bedgraph" == file_ext:
+                        print("Converting file: " + pair[i])
+                        genome = linkage_frame.loc[linkage_frame["file_path"] ==
+                                                   pair[i]]["genome"]
+                        # Find the right chrom.sizes file for genome
+                        chrom_sizes = [el for el in chrom_sizes_paths if
+                                       genome.iloc[0] in el]
                         bw_file_path = convert_bedgraph_to_bigwig(pair[i],
-                                                                chrom_sizes[0],
-                                                                conversion_tool)
-                    if os.path.exists(bw_file_path):
-                        tmp_pair.append(bw_file_path)
-                        converted_idxs.append([idx, i])
+                                                                  chrom_sizes[0],
+                                                                  conversion_tool)
+                        if not os.path.exists(bw_file_path):
+                            sort_file(pair[i])
+                            bw_file_path = convert_bedgraph_to_bigwig(pair[i],
+                                                                    chrom_sizes[0],
+                                                                    conversion_tool)
+                        if os.path.exists(bw_file_path):
+                            tmp_pair.append(bw_file_path)
+                            converted_idxs.append([idx, i])
+                        else:
+                            logging.error("The file {} could not be converted to "
+                                          "bigWig.".format(pair[i]))
                     else:
-                        logging.error("The file {} could not be converted to "
-                                      "bigWig.".format(pair[i]))
+                        tmp_pair.append(pair[i])
                 else:
-                    tmp_pair.append(pair[i])
+                    logging.error("The file {} does not exist or the path is "
+                                  "incorrect.".count(pair[i]))
+            if len(tmp_pair) == 2:
+                pairs_merge.append(tmp_pair)
+
+        # Merge all file pairs with tool and save paths of merged files
+        print("------ Merging pairs ------")
+
+        for idx, pair in enumerate(pairs_merge):
+            print("Merging pair {0} of {1}: {2}, {3}".format(idx + 1,
+                  len(pairs_merge), pair[0], pair[1]))
+
+            merged_path = merge_pair(pair[0], pair[1], merge_tool)
+            if os.path.exists(merged_path):
+                merged_files.append(merged_path)
             else:
-                logging.error("The file {} does not exist or the path is "
-                              "incorrect.".count(pair[i]))
-        if len(tmp_pair) == 2:
-            pairs_merge.append(tmp_pair)
+                logging.error("The files {0} and {1} could not be "
+                              "merged.".format(pair[0], pair[1]))
 
-    # Merge all file pairs with tool and save paths of merged files
-    print("------ Merging pairs ------")
+        # Convert merged files to bigWig format if bedGraph is not an allowed format
+        allowed_file_formats = [file_format.lower() for file_format in
+                                allowed_file_formats]
+        if "bedgraph" not in allowed_file_formats and ("bigwig" in
+           allowed_file_formats or "bw" in allowed_file_formats):
+            print("------ Converting merged files to bigWig ------")
 
-    for idx, pair in enumerate(pairs_merge):
-        print("Merging pair {0} of {1}: {2}, {3}".format(idx + 1,
-              len(pairs_merge), pair[0], pair[1]))
+            tmp_paths = []
+            for j in range(0, len(merged_files)):
+                print("Converting file {0} of {1}: {2}".format(j + 1,
+                      len(merged_files), merged_files[j]))
 
-        merged_path = merge_pair(pair[0], pair[1], merge_tool)
-        if os.path.exists(merged_path):
-            merged_files.append(merged_path)
-        else:
-            logging.error("The files {0} and {1} could not be "
-                          "merged.".format(pair[0], pair[1]))
-
-    # Convert merged files to bigWig format if bedGraph is not an allowed format
-    allowed_file_formats = [file_format.lower() for file_format in
-                            allowed_file_formats]
-    if "bedgraph" not in allowed_file_formats and ("bigwig" in
-       allowed_file_formats or "bw" in allowed_file_formats):
-        print("------ Converting merged files to bigWig ------")
-
-        tmp_paths = []
-        for j in range(0, len(merged_files)):
-            print("Converting file {0} of {1}: {2}".format(j + 1,
-                  len(merged_files), merged_files[j]))
-
-            genome = linkage_frame.loc[linkage_frame["file_path"] ==
-                                       pairs[j][0]]["genome"]
-            chrom_sizes = [el for el in chrom_sizes_paths if
-                           genome.iloc[0] in el]
-            bw_file_path = convert_bedgraph_to_bigwig(merged_files[j],
-                                                      chrom_sizes[0],
-                                                      conversion_tool)
-            if not os.path.exists(bw_file_path):
-                sort_file(merged_files[j])
+                genome = linkage_frame.loc[linkage_frame["file_path"] ==
+                                           pairs[j][0]]["genome"]
+                chrom_sizes = [el for el in chrom_sizes_paths if
+                               genome.iloc[0] in el]
                 bw_file_path = convert_bedgraph_to_bigwig(merged_files[j],
                                                           chrom_sizes[0],
                                                           conversion_tool)
-            if os.path.exists(bw_file_path):
-                tmp_paths.append(bw_file_path)
-            else:
-                logging.error("The file {} could not be converted to "
-                              "bigWig.".format(merged_files[j]))
-            old_files.append(merged_files[j])
-        merged_files = tmp_paths
+                if not os.path.exists(bw_file_path):
+                    sort_file(merged_files[j])
+                    bw_file_path = convert_bedgraph_to_bigwig(merged_files[j],
+                                                              chrom_sizes[0],
+                                                              conversion_tool)
+                if os.path.exists(bw_file_path):
+                    tmp_paths.append(bw_file_path)
+                else:
+                    logging.error("The file {} could not be converted to "
+                                  "bigWig.".format(merged_files[j]))
+                old_files.append(merged_files[j])
+            merged_files = tmp_paths
 
-    # Check if there are any bedGraph merged files that were converted to
-    # bigWig and delete them since they are no longer needed
-    print("------ Deleting old files ------")
+        # Check if there are any bedGraph merged files that were converted to
+        # bigWig and delete them since they are no longer needed
+        print("------ Deleting old files ------")
 
-    if len(old_files) > 0:
-        for idx, file in enumerate(old_files):
-            print("Deleting file {0} of {1}".format(idx + 1, len(old_files)))
-            delete_file(file)
+        if len(old_files) > 0:
+            for idx, file in enumerate(old_files):
+                print("Deleting file {0} of {1}".format(idx + 1, len(old_files)))
+                delete_file(file)
 
-    # Delete all forward/reverse files in bedGraph format now that they have
-    # been converted to bigWig
-    cnt = 1
-    for pair in pairs:
-        for k in range(0, len(pair)):
-            print("Deleting file {0} of {1}".format(cnt, len(pairs)*2))
-            delete_file(pair[k])
+        # Delete all forward/reverse files in bedGraph format now that they have
+        # been converted to bigWig
+        cnt = 1
+        for pair in pairs:
+            for k in range(0, len(pair)):
+                print("Deleting file {0} of {1}".format(cnt, len(pairs)*2))
+                delete_file(pair[k])
+                cnt += 1
+
+        # Delete all forward/reverse files in bigWig format now that they have
+        # been merged
+        cnt = 1
+        for idx_pair in converted_idxs:
+            print("Deleting file {0} of {1}".format(cnt, len(converted_idxs)))
+            delete_file(pairs_merge[idx_pair[0]][idx_pair[1]])
             cnt += 1
 
-    # Delete all forward/reverse files in bigWig format now that they have
-    # been merged
-    cnt = 1
-    for idx_pair in converted_idxs:
-        print("Deleting file {0} of {1}".format(cnt, len(converted_idxs)))
-        delete_file(pairs_merge[idx_pair[0]][idx_pair[1]])
-        cnt += 1
+        print("------ Appending entries for merged files to linking table ------")
 
-    print("------ Appending entries for merged files to linking table ------")
+        # Make rows to append to linkage table .csv file for merged files
+        for m in range(0, len(merged_files)):
+            row = pd.DataFrame(linkage_frame.loc[linkage_frame['file_path'] ==
+                                                 pairs[m][0]])
+            row['file_path'] = merged_files[m]
+            row['filename'] = os.path.basename(merged_files[m])
+            rows.append(row)
 
-    # Make rows to append to linkage table .csv file for merged files
-    for m in range(0, len(merged_files)):
-        row = pd.DataFrame(linkage_frame.loc[linkage_frame['file_path'] ==
-                                             pairs[m][0]])
-        row['file_path'] = merged_files[m]
-        row['filename'] = os.path.basename(merged_files[m])
-        rows.append(row)
+        # Concat rows to a tmp data frame and then append that to the linkage
+        # table .csv
+        if len(rows) > 0:
+            tmp_frame = rows[0]
 
-    # Concat rows to a tmp data frame and then append that to the linkage
-    # table .csv
-    if len(rows) > 0:
-        tmp_frame = rows[0]
+            for n in range(1, len(rows)):
+                tmp_frame = pd.concat([tmp_frame, rows[n]])
 
-        for n in range(1, len(rows)):
-            tmp_frame = pd.concat([tmp_frame, rows[n]])
-
-        tmp_frame.to_csv(linkage_table_path, sep=';', index=False, header=False,
-                         mode='a')
+            tmp_frame.to_csv(linkage_table_path, sep=';', index=False,
+                             header=False, mode='a')
 
 
 def read_linkage_table(linkage_table_path):
